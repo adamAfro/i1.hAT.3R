0fa2ab7e5d15dead4acc06a579c5d869691951fb
Adam Jakubczak <adam.per.jakubczak@gmail.com>
2023-11-11 15:02:30 +0100

    dokumentacja testów liniowości

--- R/liniowość.R
+++ R/liniowość.R
@@ -1,4 +1,9 @@
-#' Sprawdza założenie liniowości modelu i zwraca ramkę z wynikami
+#' @title Testy założenia liniowości
+#' @description Funkcja wykonuje testy liniowości na podanym modelu. Dostępne testy to: "Rainbow", "RESET" i "Harvey-Collier", patrz `lmtest`.
+#' @param model Model liniowy, na którym mają być wykonane testy.
+#' @param testy Wektor tekstowy z nazwami testów do wykonania. Domyślnie są to wszystkie dostępne testy.
+#' @return Ramka danych z wynikami testów. Kolumny to: "statystyka", "st. swobody [1]", "st. swobody [2]" nie dotyczy Harv.-Coll., "p". Wiersze to nazwy wykonanych testów.
+#' @export
 liniowość <- function(model, testy = c("Rainbow", "RESET", "Harvey-Collier")) {
 
     wyniki = data.frame()
